eb9a71dc8d157d4861318c9e891865be5581bc16
Egor_Kozlyakovsky1 <139443070+Egor-Git123@users.noreply.github.com>
2023-11-18 21:07:41 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,65 +1,18 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 import sys
-from PyQt5.QtGui import QIcon, QPainter, QPen, QBrush, QColor
+from PyQt5.QtGui import QIcon, QPainter, QPen, QBrush
 import io
 from PyQt5 import uic
 import random
 
-
-template = """<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>MainWindow</class>
- <widget class="QMainWindow" name="MainWindow">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>800</width>
-    <height>600</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>MainWindow</string>
-  </property>
-  <widget class="QWidget" name="centralwidget">
-   <widget class="QPushButton" name="pushButton">
-    <property name="geometry">
-     <rect>
-      <x>300</x>
-      <y>30</y>
-      <width>151</width>
-      <height>61</height>
-     </rect>
-    </property>
-    <property name="text">
-     <string>PushButton</string>
-    </property>
-   </widget>
-  </widget>
-  <widget class="QMenuBar" name="menubar">
-   <property name="geometry">
-    <rect>
-     <x>0</x>
-     <y>0</y>
-     <width>800</width>
-     <height>26</height>
-    </rect>
-   </property>
-  </widget>
-  <widget class="QStatusBar" name="statusbar"/>
- </widget>
- <resources/>
- <connections/>
-</ui>
-"""
+from ui_fil import Ui_MainWindow
 
 
-class Window(QMainWindow):
+class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.setupUi(self)
 
         self.flag = False
 
@@ -72,7 +25,7 @@ class Window(QMainWindow):
     def paintEvent(self, event):
         if self.flag:
             painter = QPainter(self)
-            painter.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+            painter.setBrush(QBrush(Qt.yellow))
             c = random.randint(100, 200)
             painter.drawEllipse(40, 40, c, c)
 
